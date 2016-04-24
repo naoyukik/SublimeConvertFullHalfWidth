931591a817b132af0102b6bdbc3cd9d0a7a6eb51
naoyukik <mmaga+github@dstribe.net>
2016-04-24 22:38:35 +0900

    change module jctconv to jaconv

--- convertZenHan.py
+++ convertZenHan.py
@@ -1,17 +1,16 @@
-import sublime
 import sublime_plugin
-from . import jctconv
+from .jaconv.jaconv import h2z, z2h
+
 
 class ConvertZenHanCommand(sublime_plugin.TextCommand):
     def run(self, edit, to, target):
-        option = 0
+        # option = 0
         sels = self.view.sel()
-        print(target[0])
         for sel in sels:
             selection_word = self.view.substr(sel)
             if to == 'zen':
-                result = jctconv.h2z(selection_word, target[0])
+                result = h2z(selection_word, target[0])
             else:
-                result = jctconv.z2h(selection_word, target[0])
+                result = z2h(selection_word, target[0])
 
             self.view.replace(edit, sel, result)
