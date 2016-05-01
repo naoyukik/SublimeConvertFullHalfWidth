2dc95ec26fc3e5ab9f1c5694285c123f32dbe478
naoyukik <mmaga+github@dstribe.net>
2016-05-01 17:51:27 +0900

    jaconvの関数呼び出しに対応

--- convertZenHan.py
+++ convertZenHan.py
@@ -3,14 +3,13 @@ from .jaconv.jaconv import h2z, z2h
 
 
 class ConvertZenHanCommand(sublime_plugin.TextCommand):
-    def run(self, edit, to, target):
-        # option = 0
+    def run(self, edit, to, kana=False, ascii=False, digit=False):
         sels = self.view.sel()
         for sel in sels:
             selection_word = self.view.substr(sel)
             if to == 'zen':
-                result = h2z(selection_word, target[0])
+                result = h2z(selection_word, '', kana, ascii, digit)
             else:
-                result = z2h(selection_word, target[0])
+                result = z2h(selection_word, '', kana, ascii, digit)
 
             self.view.replace(edit, sel, result)
